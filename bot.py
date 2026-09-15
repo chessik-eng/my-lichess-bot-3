1243765046164953c8dac8145616e404ef524d0c
chessik-eng <chessik11@ukr.net>
2026-09-15 19:25:26 +0300

    Add project structure and file references in bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,7 @@
-
+my-lichess-bot-3
+├── README.md
+├── requirements.txt   ← зависимости
+├── bot.py              ← КОД БОТА
+└── .github/
+    └── workflows/
+        └── bot.yml     ← запуск в облаке
