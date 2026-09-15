6106cb7cea93ad4c45659ab66c645fccb565e3a1
chessik-eng <chessik11@ukr.net>
2026-09-15 19:26:25 +0300

    Remove project structure comments from bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,1 @@
-my-lichess-bot-3
-├── README.md
-├── requirements.txt   ← зависимости
-├── bot.py              ← КОД БОТА
-└── .github/
-    └── workflows/
-        └── bot.yml     ← запуск в облаке
+
